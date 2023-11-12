b00b9da2a339b3395bbfb2a8a11e193e75e6489c
spenido <150428049+spenido@users.noreply.github.com>
2023-11-12 07:22:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-document.getElementById('submit-button').addEventListener('click', function () {
-    const inputPassword = document.getElementById('password').value;
-    const fixedPassword = 'sproduction#23'; // Replace with your fixed password
+var expectedPassword = "sproduction#23";
+var storageKey = "passwordEnteredToday";
 
-    if (inputPassword === fixedPassword) {
-        document.getElementById('login-message').textContent = 'Login successful!';
-        document.getElementById('loginSect').style.display = 'none';
+// Check if the password has been entered today
+if (localStorage.getItem(storageKey)) {
+    document.getElementById("mainSession").style.display = "block";
+    document.getElementById("securitySession").style.display = "none";
+} else {
+    document.getElementById("mainSession").style.display = "none";
+    document.getElementById("securitySession").style.display = "flex";
+}
+
+function checkPassword() {
+    var enteredPassword = document.getElementById("password").value;
+    var messageElement = document.getElementById("message");
+
+    if (enteredPassword === expectedPassword) {
+        messageElement.innerHTML = "Password benar. Anda dapat melanjutkan.";
+        messageElement.style.color = "green";
+        
+        // Set a flag in localStorage to indicate that the password has been entered today
+        localStorage.setItem(storageKey, "true");
+
+        // Hide the security session and show the main session
+        document.getElementById("securitySession").style.display = "none";
+        document.getElementById("mainSession").style.display = "block";
     } else {
-        document.getElementById('login-message').textContent = 'Login failed. Incorrect password!';
+        messageElement.innerHTML = "Password salah. Silakan coba lagi.";
+        messageElement.style.color = "red";
     }
-});
-
-document.getElementById('login-form').addEventListener('input', function() {
-    document.getElementById('label').style.display = "none";
-  });
-  
+}
